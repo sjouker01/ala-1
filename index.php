add9200ba39294a3339f4eaa4fa1d36e088fc004
sjouk jonker <sjoukjonker13@gmail.com>
2023-09-22 16:30:52 +0200

    jdjhd

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link  rel="stylesheet" href="stylefolder\style.css">
 </head>
 <body>
-    
+    <?php require_once 'stylefolder\inlogen.php'; ?>
+    <h2>Inloggen</h2>
+    <form action="login.php" method="post">
+        Gebruikersnaam: <input type="text" name="gebruikersnaam"><br>
+        Wachtwoord: <input type="password" name="wachtwoord"><br>
+        <input type="submit" value="Inloggen">
+    </form>
 </body>
 </html>
