0500adef6bc4b9b88cd6135d2b21e84604b5cd55
sjouk jonker <sjoukjonker13@gmail.com>
2023-09-25 11:14:23 +0200

    update

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
     <?php 
        require_once("stylefolder\conecting.php");
+       require_once("stylefolder\header.php");
 
-   
+       
+     require_once("stylefolder\hooter.php");
     ?>
     
 </body>
