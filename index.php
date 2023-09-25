6d313700225327361d502ef1547f61a1d9930171
sjouk jonker <sjoukjonker13@gmail.com>
2023-09-25 08:40:30 +0200

    conecting

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>Document</title>
 </head>
 <body>
+    <?php 
+       require_once("stylefolder/connecting.php");
+
+       if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "<p>Connected successfully</p>";
+    }
+    ?>
     
 </body>
 </html>
