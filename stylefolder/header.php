8202807cf21e035c0eeb30521aa001534dad774b
Thumaes <118362290+Thumaes@users.noreply.github.com>
2023-10-26 21:21:05 +0200

    header veranderingen
    header met style, geen idee of het goed is dat ga ik nu controleren

--- stylefolder/header.php
+++ stylefolder/header.php
@@ -8,17 +8,89 @@
 </head>
 <body>
 <!-- hier komt header -->
+
+<style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 0;
+        }
+
+        nav ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        nav ul li {
+            display: inline;
+            margin-right: 20px;
+        }
+
+        nav ul li a {
+            text-decoration: none;
+            color: #fff;
+        }
+
+        .search-bar {
+            margin-top: 10px;
+        }
+
+        .search-bar input[type="text"] {
+            padding: 5px;
+            border: 1px solid #ccc;
+        }
+
+        .search-bar button {
+            background-color: #555;
+            color: #fff;
+            border: none;
+            padding: 5px 10px;
+        }
+
+        .search-bar button:hover {
+            background-color: #777;
+        }
+
+        a {
+            text-decoration: none;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
 <header>
- <?php
-require_once("conecting.php");
+    <nav>
+        <ul>
+            <li><a href="persoonlijk.php">Persoonlijk</a></li>
+            <li><a href="warehouse.php">Warehouse</a></li>
+            <li><a href="toevoegen.php">Toevoegen</a></li>
+            <li><a href="home.php">Home</a></li>
+        </ul>
+    </nav>
+
+    <div class="search-bar">
+        <form action="search.php" method="get">
+            <input type="text" name="query" placeholder="Search...">
+            <button type="submit">Search</button>
+        </form>
+    </div>
+
+    <?php
+        require_once("conecting.php");
     ?>
 
-    <a href="stylefolder\login.php"> login</a>
+    <a href="stylefolder\login.php">Login</a>
 
-    <?php session_start(); 
-    if(isset($_SESSION["username"])){
-      ?><a  href="stylefolder\ingelogd.php"> <?php echo $_SESSION["username"];?></a><?php
-    }
+    <?php
+        session_start();
+        if(isset($_SESSION["username"])){
+            echo '<a href="stylefolder\ingelogd.php">' . $_SESSION["username"] . '</a>';
+        }
     ?>
 </header>
     
